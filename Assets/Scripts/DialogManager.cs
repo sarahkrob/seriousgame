12a0947aa76fe98431a626046626a1b37913ed9e
u1324575 <theo.grant9@gmail.com>
2020-10-06 12:18:52 -0600

    starting files

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    public Text dialog;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void startD(Dialog d)
+    {
+        sentences.Clear();
+        foreach(string sent in d.sentences)
+        {
+            sentences.Enqueue(sent);
+        }
+
+        DisplayNextSent();
+    }
+
+    public void DisplayNextSent()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+        string returnSentence = sentences.Dequeue();
+        dialog.text = returnSentence;
+
+    }
+
+    void EndDialog()
+    {
+
+    }
+}
---
+++ Assets/Scripts/DialogTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogTrigger : MonoBehaviour
+{
+
+    public Dialog d;
+
+    public void TriggerDialog()
+    {
+        FindObjectOfType<DialogManager>().startD(d);
+    }
+
+}
